19bb837ca281f4cbf72cb3e476951903cca3048b
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2024-07-11 10:29:01 +0100

    Complete exercise 3.4

--- 03-message-broker/04-nack/main.go
+++ 03-message-broker/04-nack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ThreeDotsLabs/watermill/message"
 )
@@ -18,6 +19,24 @@ func ConsumeMessages(sub message.Subscriber, alarmClient AlarmClient) {
 	}
 
 	for msg := range messages {
+		switch string(msg.Payload) {
+		case "0":
+			if err := alarmClient.StopAlarm(); err != nil {
+				fmt.Printf("failed to stop alarm: %s\n", err)
+				msg.Nack()
+				continue
+			}
+		case "1":
+			if err := alarmClient.StartAlarm(); err != nil {
+				fmt.Printf("failed to start alarm: %s\n", err)
+				msg.Nack()
+				continue
+			}
+		default:
+			fmt.Printf("unknown payload: %s\n", string(msg.Payload))
 
+		}
+
+		msg.Ack()
 	}
 }
