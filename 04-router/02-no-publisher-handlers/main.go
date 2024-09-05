e50d81f316080923db1660dd5dc8a3b0309bbc4b
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2024-09-05 17:53:52 +0100

    Complete exercise 4.2

---
+++ 04-router/02-no-publisher-handlers/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewStdLogger(false, false)
+
+	router, err := message.NewRouter(message.RouterConfig{}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	sub, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	router.AddNoPublisherHandler(
+		"celsius-to-fahrenheit",
+		"temperature-fahrenheit",
+		sub,
+		func(msg *message.Message) error {
+			fmt.Printf("Temperature read: %s\n", msg.Payload)
+			return nil
+		},
+	)
+
+	err = router.Run(context.Background())
+	if err != nil {
+		panic(err)
+	}
+}
