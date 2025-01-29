278cac7cd06ebd24efe2ce3bcaeaabc15bc10859
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2025-01-29 15:52:53 +0000

    Complete exercise 11.4

---
+++ 11-outbox/04-publishing-event-in-transaction/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/ThreeDotsLabs/watermill"
+	watermillSQL "github.com/ThreeDotsLabs/watermill-sql/v2/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/message"
+	_ "github.com/lib/pq"
+)
+
+func PublishInTx(
+	message *message.Message,
+	tx *sql.Tx,
+	logger watermill.LoggerAdapter,
+) error {
+	publisher, err := watermillSQL.NewPublisher(
+		tx,
+		watermillSQL.PublisherConfig{
+			SchemaAdapter: watermillSQL.DefaultPostgreSQLSchema{},
+		},
+		logger,
+	)
+	if err != nil {
+		return fmt.Errorf("creating publisher: %w", err)
+	}
+
+	if err := publisher.Publish("ItemAddedToCart", message); err != nil {
+		return fmt.Errorf("publishing: %w", err)
+	}
+
+	return nil
+}
